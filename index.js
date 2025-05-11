6f0c7087ebf57773b716c07a1157247474edaf10
Sean <140343656+philsean@users.noreply.github.com>
2025-05-11 15:17:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require('express')
-      axios   = require('axios')
-      path    = require('path')
+const express = require('express'),
+      axios   = require('axios'),
+      path    = require('path'),
       app     = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+for (const v of fs.readdirSync('./src/api/')) {
+  let api = require(`./src/api/${v}/index.js`);
+  app.use(`/api/${v}`, api);
+}
 
 app.get('/', (req, res) => {
   res.send('<h1>E é isso veyr, vivências veyr</h1>');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`[ Express ] › All right! ( ${process.env.PORT} )`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`[ Express ] › All right! ( ${process.env.PORT || 5000} )`);
 });
 
 module.exports = app;
