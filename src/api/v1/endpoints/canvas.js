73225d2190858c419498cf7d89a65ef3272d5237
Sean <140343656+philsean@users.noreply.github.com>
2025-05-12 20:21:38 -0300

    Update canvas.js

--- src/api/v1/endpoints/canvas.js
+++ src/api/v1/endpoints/canvas.js
@@ -52,7 +52,7 @@ const shapeHandlers = {
     const { url, x, y, width, height } = parts
     const response = await axios.get(decodeURIComponent(url), { responseType: 'arraybuffer' })
     const img = await loadImage(Buffer.from(response.data))
-    
+    console.log(img)
     ctx.drawImage(img, parseInt(x || 0), parseInt(y || 0), parseInt(width || img.width), parseInt(height || img.height))
     }
   }
